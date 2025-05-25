c99ea6406e1e58c760d9c6078f10a444d8e599c6
Vitalina <vsinkovaa@gmail.com>
2025-05-25 18:10:56 +0300

    install spiner

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,21 +1,30 @@
-import type { ReactElement } from 'react';
+import { useState, type ReactElement, type CSSProperties} from 'react';
 import css from './Loader.module.css';
-// import { RotatingTriangles } from 'react-loader-spinner'
+import { RingLoader } from 'react-spinners'
   
 export default function Loader(): ReactElement {
+    const [loading] = useState(true);
+    const color = '#0000FF';
+
+    const override: CSSProperties = {
+        display: 'block',
+        margin: '0 auto',
+        borderColor: 'color',
+    };
+
     return (
         <p className={css.text}
             role='status'
             aria-busy='true'
         >
-            {/* <RotatingTriangles
-        visible={true}
-        height='80'
-        width='80' 
-        ariaLabel='rotating-triangles-loading'
-        wrapperStyle={{}}
-        wrapperClass={''}
-            /> */}
+            <RingLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={80}
+        aria-label='Loading Spinner'
+        data-testid='loader'
+            />
             Loading movies, please wait...
         </p>
     );
