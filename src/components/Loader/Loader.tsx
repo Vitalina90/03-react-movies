ea573bd17e5a0d04c928ad3cb736074279f501ed
Vitalina <vsinkovaa@gmail.com>
2025-05-25 14:30:24 +0300

    spiner

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
 import type { ReactElement } from 'react';
 import css from './Loader.module.css';
-import { RotatingTriangles } from 'react-loader-spinner'
+// import { RotatingTriangles } from 'react-loader-spinner'
   
 export default function Loader(): ReactElement {
     return (
@@ -8,14 +8,14 @@ export default function Loader(): ReactElement {
             role='status'
             aria-busy='true'
         >
-            <RotatingTriangles
+            {/* <RotatingTriangles
         visible={true}
         height='80'
         width='80' 
         ariaLabel='rotating-triangles-loading'
         wrapperStyle={{}}
         wrapperClass={''}
-            />
+            /> */}
             Loading movies, please wait...
         </p>
     );
